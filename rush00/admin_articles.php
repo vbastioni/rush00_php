ab0c7a301f7b4ce4972236a64d9126a18f0961f9
Celine LUCAS <clucas@e3r1p1.42.fr>
2018-06-02 18:20:22 +0200

    ajouts reste pages admin

---
+++ rush00/admin_articles.php
@@ -0,0 +1,38 @@
+<?PHP
+include "header.php";
+?>
+<h1>Gestion des articles dans "<?php echo $categorie['name']; ?>" (<a href='admin_categories.php'>Retour</a>)</h1><br />
+<?PHP
+if (isset($msg)) {
+	echo "<p>".$msg."</p>";
+}
+?>
+<table>
+<?PHP
+$articles = mysqli_query($sql, "SELECT * FROM articles WHERE id_category = ".$id_categorie." ORDER BY id");
+while ($data = mysqli_fetch_array($articles)) {
+	?>
+	<tr>
+		<td>#<?php echo $data['id']; ?></td>
+		<td><img alt='<?php echo $data['name']; ?>' src='<?php echo $data['photo']; ?>' title='<?php echo $data['name']; ?>' /></td>
+		<td>
+			<form method="post" action="admin_articles.php?id=<?php echo $id_categorie; ?>">
+				<input type="text" name="id" value="<?php echo $data['id']; ?>" style="display:none;" />
+				Nom: <input type="text" name="name" value="<?php echo $data['name']; ?>" />
+				<br />
+				URL Photo: <input type="text" name="photo" value="<?php echo $data['photo']; ?>" />
+				<br />
+				Prix: <input type="text" name="price" value="<?php echo $data['price']; ?>" />
+				<br />
+				<input type="submit" name="modif" value="Valider les changements" />
+			</form>
+		</td>
+		<td><a onclick="return confirm('Voulez-vous vraiment supprimer cet article? Cette action est irreversible !');" href='admin_articles.php?id=<?php echo $id_categorie; ?>&del_id=<?php echo $data['id']; ?>'>Supprimer</a></td>
+	</tr>
+	<?PHP
+}
+?>
+</table>
+<?PHP
+include "footer.php";
+?>
