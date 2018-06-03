cb6ed925a691d002f7ca2da7c577bb2b56973ac3
Celine LUCAS <clucas@e3r1p1.42.fr>
2018-06-03 12:20:36 +0200

    db ok

--- rush00/install.php
+++ rush00/install.php
@@ -13,12 +13,16 @@
 $sql = mysqli_connect('192.168.99.100:3306', 'root', 'root'); 
 if (!$sql)
     die("Connection Failed:".mysqli_connect_error());
+    
 mysqli_query($sql, "CREATE DATABASE IF NOT EXISTS ft_minishop");
 mysqli_query($sql, "USE ft_minishop");
 
-mysqli_query($sql, "CREATE TABLE cart
+mysqli_query($sql, "CREATE TABLE panier
 (
-    id int(11) NOT NULL AUTO_INCREMENT,
+    id int(6) NOT NULL AUTO_INCREMENT,
+    id_user int(6) NOT NULL,
+    content text NOT NULL,
+    finished int(6) NOT NULL,
     pdt_id int(11) NOT NULL,
     pdt_qty int(11) NOT NULL,
     pdt_cost int(11) NOT NULL,
@@ -33,38 +37,27 @@ mysqli_query($sql, "CREATE TABLE users
 (
     id int(11) NOT NULL AUTO_INCREMENT,
     email text NOT NULL,
-    passwd text NOT NULL,
-    is_admin bit NOT NULL,
+    password text NOT NULL,
+    admin bit NOT NULL,
     desactivated bit NOT NULL,
-    id_cart int(11) NOT NULL,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1");
 
-mysqli_query($sql, "CREATE TABLE categori
-(
+mysqli_query($sql, "CREATE TABLE categories (
     id int(11) NOT NULL AUTO_INCREMENT,
+    name text NOT NULL,
     PRIMARY KEY (id)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1");
+
+mysqli_query($sql, "CREATE TABLE articles (
+	id int(11) NOT NULL AUTO_INCREMENT,
+	id_category int(11) NOT NULL,
+	name text NOT NULL,
+	photo text NOT NULL,
+	price int(11) NOT NULL,
+	PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1");
 
-mysqli_query($sql, "CREATE TABLE category
-(
-    id int(11) NOT NULL AUTO_INCREMENT,
-    cat_name text NOT NULL,
-    PRIMARY KEY (id)--,
-    -- CONSTRAINT name_unique UNIQUE (cat_name)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1");
-
-mysqli_query($sql, "CREATE TABLE articles
-(
-    id int(11) NOT NULL AUTO_INCREMENT,
-    id_category int(11) NOT NULL,
-    id_type int(11) NOT NULL,
-    pdt_name text NOT NULL,
-    photo text NOT NULL,
-    price int(11) NOT NULL,
-    PRIMARY KEY (id)--,
-    -- CONSTRAINT name_unique UNIQUE (name)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1");
 
 /*  INSERTION INTO TABLES   */
 
@@ -77,8 +70,8 @@ mysqli_query($sql, "INSERT INTO categories (name) VALUES ('world')");
 
 /*  INSERT ARTICLES */
 
-mysqli_query($sql, "INSERT INTO articles VALUES (NULL, 124, 'Woodstock', 'https://media.cdnws.com/_i/25864/2304/1160/7/skateboard-skate-penny-nickel-complete-27-tie-dye-die-multicolore-flashy-pouces-pas-cher-woodstock.png', 152)");
-mysqli_query($sql, "INSERT INTO articles VALUES (NULL, 124, 'Rasta', 'http://www.boardridersguide.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/p/e/penny-skateboard-rasta.jpg', 140)");
+mysqli_query($sql, "INSERT INTO articles VALUES (NULL, 1, 'Woodstock', 'https://media.cdnws.com/_i/25864/2304/1160/7/skateboard-skate-penny-nickel-complete-27-tie-dye-die-multicolore-flashy-pouces-pas-cher-woodstock.png', 152)");
+mysqli_query($sql, "INSERT INTO articles VALUES (NULL, 1, 'Rasta', 'http://www.boardridersguide.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/p/e/penny-skateboard-rasta.jpg', 140)");
 
 /*  INSERT USERS    */
 
@@ -87,4 +80,4 @@ mysqli_query($sql, "INSERT INTO users VALUES (NULL, 'vbastion@student.42.fr', 'm
 
 echo "database ok";
 
-?> 
+?>
